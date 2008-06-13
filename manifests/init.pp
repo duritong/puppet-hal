b45cf0c657f07cf0d0076944091e15a84cb76af5
mh <mh@immerda.ch>
2008-06-13 19:05:00 +0000

    Merge commit 'puzzle/development'

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,11 +1,18 @@
-#######################################
+#
 # hal module
-# Puzzle ITC - haerry+puppet(at)puzzle.ch
-# GPLv3
-#######################################
-
+#
+# Copyright 2008, Puzzle ITC GmbH
+# Marcel Härry haerry+puppet(at)puzzle.ch
+# Simon Josi josi+puppet(at)puzzle.ch
+#
+# This program is free software; you can redistribute 
+# it and/or modify it under the terms of the GNU 
+# General Public License version 3 as published by 
+# the Free Software Foundation.
+#
 
 # modules_dir { "hal": }
+
 class hal {
     include hal::base
 }
